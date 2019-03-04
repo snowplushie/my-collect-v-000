9df4cb33fc8c894a63281a425e51f8184044ef1d
Paige Wiener <snowplushie@yahoo.com>
2019-03-04 03:25:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
-def my_collect(languages)
+def my_collect(array)
   i = 0
-  while i < languages.length
-  yield(languages[i])
+  while i < array.length
+  yield(array[i])
   i+=1
 end
 end
 
 languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
